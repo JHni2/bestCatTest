2a21797d537fe7dd5dd70bfdb61e676881ea8977
Jihyun <wlgus0260@naver.com>
2023-02-06 22:08:15 +0900

    테스트 로직 만들기

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,15 +1,74 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 import { QuestionData } from '../stores/Question/QuestionData';
 import Header from '../components/Header';
-import { Button } from 'react-bootstrap';
 
 export default function QuestionPage(): React.ReactElement {
-  const [questionNo, setQuestionNo] = React.useState(0);
+  const navigate = useNavigate();
 
-  const handleClickAnswer = () => {
+  const [questionNo, setQuestionNo] = React.useState(0);
+  const [totalScore, setTotalScore] = React.useState([
+    {
+      id: 'EI',
+      score: 0,
+    },
+    {
+      id: 'SN',
+      score: 0,
+    },
+    {
+      id: 'TF',
+      score: 0,
+    },
+    {
+      id: 'JP',
+      score: 0,
+    },
+  ]);
+
+  const handleClickAnswer = (ans: number, type: string) => {
     setQuestionNo(questionNo + 1);
+
+    const newScore = totalScore.map((s) =>
+      s.id === type ? { id: s.id, score: s.score + ans } : s,
+    );
+
+    setTotalScore(newScore);
+
+    // 마지막 문제가 아닐 경우
+    if (QuestionData.length !== questionNo + 1) {
+      setQuestionNo(questionNo + 1);
+    }
+    // 마지막 문제일 경우
+    else {
+      navigate('/result');
+    }
+
+    /* newScore과 같은 코드
+    if (type === 'EI') {
+      // 기존 스코어에 더한 새로운 스코어 값
+      const addScore = totalScore[0].score + ans;
+      // 새로운 스코어를 반영한 새로운 객체
+      const object = { id: 'EI', score: addScore };
+      // 새로운 객체를 기존에 토탈 스코어 반영
+      totalScore.splice(0, 1, object);
+    } else if (type === 'SN') {
+      const addScore = totalScore[0].score + ans;
+      const object = { id: 'SN', score: addScore };
+      totalScore.splice(0, 1, object);
+    } else if (type === 'TF') {
+      const addScore = totalScore[0].score + ans;
+      const object = { id: 'TF', score: addScore };
+      totalScore.splice(0, 1, object);
+    } else {
+      const addScore = totalScore[0].score + ans;
+      const object = { id: 'JP', score: addScore };
+      totalScore.splice(0, 1, object);
+    }
+    */
   };
 
   return (
@@ -20,12 +79,25 @@ export default function QuestionPage(): React.ReactElement {
         <ButtonGroup>
           <Button
             className="btn-warning"
-            style={{ marginRight: '20px' }}
-            onClick={handleClickAnswer}
+            style={{
+              marginRight: '20px',
+              width: '45%',
+              minHeight: '200px',
+              fontSize: '14pt',
+            }}
+            onClick={() => handleClickAnswer(1, QuestionData[questionNo].type)}
           >
             {QuestionData[questionNo].answera}
           </Button>
-          <Button className="btn-warning">
+          <Button
+            className="btn-warning"
+            style={{
+              width: '45%',
+              minHeight: '200px',
+              fontSize: '14pt',
+            }}
+            onClick={() => handleClickAnswer(0, QuestionData[questionNo].type)}
+          >
             {QuestionData[questionNo].answerb}
           </Button>
         </ButtonGroup>
